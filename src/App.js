4d60d2293aaddb307ffd0980877c2415d68bfc58
Dharvik19 <dharvik.badga22@gmail.com>
2023-02-26 15:31:45 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,21 +7,49 @@ import LoadingSpinner from './components/LoadingSpinner';
 function App() {
   const [movies, setMovies] = useState([]);
   const[loading, setIsLoading] = useState(false);
+  const[error, setError] = useState(null);
+  const abortController = useRef(null);
   async function fetchmoviesHandler (){
+    
     setIsLoading(true);
-   const response = await fetch('https://swapi.dev/api/films')
-   const data = await response.json();
-   
-      const transformedMovies = data.results.map(movieData=>{
-          return{
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          }
-      });
-      setMovies(transformedMovies);
+    setError(null);
+    try{
+      abortController.current = new AbortController();
+      const response = await fetch('https://swapi.dev/api/films', {
+        signal: abortController.current.signal
+      })
+      if(!response.ok){
+        setInterval(()=>{
+            try{
+              response();
+            }catch(error){
+                console.log("error");
+            }
+        },5000);
+        throw new Error('something went wrong ...RETRYING');
+       
+      }
+      const data = await response.json();
+       const transformedMovies = data.results.map(movieData=>{
+           return{
+             id: movieData.episode_id,
+             title: movieData.title,
+             openingText: movieData.opening_crawl,
+             releaseDate: movieData.release_date
+           }
+       });
+       setMovies(transformedMovies);
+      
+      } catch(error){
+        setError(error.message);
+      }
       setIsLoading(false);
+      console.log("working");
+    
+   
+  }
+  const cancel =()=>{
+    abortController.current && abortController.current.abort();
   }
   return (
     <React.Fragment>
@@ -29,8 +57,10 @@ function App() {
         <button onClick={fetchmoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!loading && <MoviesList movies={movies} />}
-        {loading && <LoadingSpinner></LoadingSpinner>}
+        {!loading && movies.length>0 && <MoviesList movies={movies} />}
+        {!loading && movies.length === 0 && !error &&<p> Found no movies</p>}
+        {!loading && error && <div><p>{error}</p> </div>}
+        {loading && <div><LoadingSpinner></LoadingSpinner>  <br/><button onClick={cancel}>cancel</button></div>}
       </section>
     </React.Fragment>
   );
